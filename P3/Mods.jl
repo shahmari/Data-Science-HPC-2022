03ac87a63425e296c3112e904b0b91eb24921b87
Yaghuob Shahmari <shahmari.acer@outlook.com>
2022-06-16 18:03:07 +0430

    Create Mods.jl

---
+++ P3/Mods.jl
@@ -0,0 +1,17 @@
+module UsedFuncs
+
+using StatsBase
+
+export NormalizeFunc, FitLine, RSS, TSS, R²
+
+NormalizeFunc(Ans::Vector) = (Ans .- mean(Ans)) / std(Ans)
+FitLine(X::Vector, Y::Vector) = [hcat(X) ones(length(X))] \ Y
+
+RSS(Ys::Vector, Xs::Vector, f::Function) = sum((Ys .- f.(Xs)) .^ 2)
+TSS(Ys::Vector) = sum((Ys .- mean(Ys)) .^ 2)
+R²(Ys::Vector, Xs::Vector, f::Function) = 1 - (RSS(Ys, Xs, f) / TSS(Ys))
+
+RSS(Ys::Vector, fx::Vector) = sum((Ys .- fx) .^ 2)
+R²(Ys::Vector, fx::Vector) = 1 - (RSS(Ys, fx) / TSS(Ys))
+
+end
